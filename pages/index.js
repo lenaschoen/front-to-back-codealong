df01beaf838d5016714de45e40202b9be4eade16
Lena Schön <lena.schoen@squer.at>
2021-10-28 19:34:32 +0200

    add database

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import { useState } from 'react';
-import { albumList, artistList, genreList } from '../assets/additionalAssets';
-import { songList } from '../assets/songList';
 import { AudioPlayer } from '../components/AudioPlayer';
 import { FilterInput } from '../components/FilterInput';
 
-export default function Home() {
-  const [activeSong, setActiveSong] = useState(songList[0]);
+export default function Home({ albums, artists, genres, songs }) {
+  const [activeSong, setActiveSong] = useState(songs[0]);
   const [genreFilter, setGenreFilter] = useState('');
   const [artistFilter, setArtistFilter] = useState('');
   const [albumFilter, setAlbumFilter] = useState('');
@@ -18,19 +16,19 @@ export default function Home() {
       <AudioPlayer activeSong={activeSong} />
       <section className="filter-section">
         <FilterInput
-          options={genreList} // genreList from assets directory
+          options={genres} // genreList from assets directory
           value={genreFilter} // genreFilter from useState()
           filterSetter={setGenreFilter} // setGenreFilter from useState()
           name="genre"
         />
         <FilterInput
-          options={artistList} // artistList from assets directory
+          options={artists} // artistList from assets directory
           value={artistFilter} // artistFilter from useState()
           filterSetter={setArtistFilter} // setArtistFilter from useState()
           name="artist"
         />
         <FilterInput
-          options={albumList} // albumList from assets directory
+          options={albums} // albumList from assets directory
           value={albumFilter} // albumFilter from useState()
           filterSetter={setAlbumFilter} // setAlbumFilter from useState()
           name="album"
@@ -45,7 +43,7 @@ export default function Home() {
           <div>Play</div>
           <div>Genre</div>
         </div>
-        {songList
+        {songs
           .filter((song) => {
             let isVisible = true; // by default all songs are visible
 
@@ -96,17 +94,20 @@ export default function Home() {
   );
 }
 
-// ⚠️⚠️⚠️
-// ⚠️⚠️⚠️ PLEASE IGNORE this section until you have completed steps  1, 2 and 3 ⚠️⚠️⚠️
-// ⚠️⚠️⚠️
-//
-// --------------- Step 4 --------------
-//
-// Now, we are going to connect our front-end and back-end.
-// Using Get Server Side Props (aka. GSSp) get the app data from the database instead of the assets files.
-//
-// 1. Import the database Util functions from utils/database.js
-// 2. Get the data for the app. (albumList, artistList, genreList, songList)
-// 3. Pass the data to the Page Component through props object
-// 4. modify the code in the Home component to receive the data from database instead of assets
-// 5. remove assets directory and the
+export async function getServerSideProps() {
+  const { getAlbums, getArtists, getGenres, getSongs } = await import(
+    '../utils/database'
+  );
+  const albums = await getAlbums();
+  const artists = await getArtists();
+  const genres = await getGenres();
+  const songs = await getSongs();
+  return {
+    props: {
+      albums,
+      artists,
+      genres,
+      songs,
+    },
+  };
+}
